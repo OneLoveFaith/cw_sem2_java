7dbc04f67d41c294be3ac4a4ee363a25282422fd
LovelyFeititi <58261454+OneLoveFaith@users.noreply.github.com>
2022-02-14 21:09:57 +0600

    made a list of functions for the employee

---
+++ src/main/java/com/pharmacy/Controllers/Worker.java
@@ -0,0 +1,44 @@
+package com.pharmacy.Controllers;
+
+import com.pharmacy.Methods.Methods;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class Worker  {
+
+    @FXML
+    private Label workerLabel;
+
+    @FXML
+    private ListView<String> WorkerListView;
+
+    @FXML
+    private Button LogOutButton;
+
+    String[] option = {"Show available medicines", "Search medicine", "Report", "Make order", "Ordered medicines", "Needed medicines", "Medicines with discount", "Delete order"};
+
+    String aktuallOption;
+
+    @FXML
+    void initialize(){
+        workerLabel.setText( "Hello, " + Login.name);
+        WorkerListView.getItems().addAll(option);
+        LogOutButton.setOnAction(event -> Methods.loadStage("Login", event));
+        WorkerListView.setOnMouseClicked(event -> {
+            aktuallOption = WorkerListView.getSelectionModel().getSelectedItem();
+            switch (aktuallOption) {
+                case "Show available medicines" -> {}
+                case "Search medicine" -> {}
+                case "Report" -> {}
+                case "Make order" -> {}
+                case "Ordered medicines" -> {}
+                case "Needed medicines" -> {}
+                case "Medicines with discount" -> {}
+                case "Delete order" -> {}
+                default -> {}
+            }
+        });
+    }
+}
