742cb1aa63896c2e75091c8de3fe778a5c9a8b8a
LovelyFeititi <58261454+OneLoveFaith@users.noreply.github.com>
2022-02-14 21:11:55 +0600

    added file structure and database file

---
+++ src/main/java/com/pharmacy/Database/Database.java
@@ -0,0 +1,21 @@
+package com.pharmacy.Database;
+
+//MongoDB imports
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class Database {
+    //Verbindung zur cloud
+    public static MongoClient client = new MongoClient(new MongoClientURI("mongodb+srv://heyhey:test@lovelyprojectsbypaipai.jp7fl.mongodb.net/test"));
+
+
+    public static MongoDatabase database = client.getDatabase("Pharmacy");
+
+    public static MongoCollection<Document> users = Database.database.getCollection("users");
+
+    public static FindIterable<Document> foundedUsers = users.find();
+}
