a95a92a3680508b46578266d73b98621365c77f5
LovelyFeititi <58261454+OneLoveFaith@users.noreply.github.com>
2022-03-18 16:01:37 +0600

    Created registration of new users

--- src/main/java/com/pharmacy/Methods/Methods.java
+++ src/main/java/com/pharmacy/Methods/Methods.java
@@ -1,9 +1,15 @@
 package com.pharmacy.Methods;
 
-import com.mongodb.client.model.Filters;
+
+//Imports
+import com.pharmacy.Classes.Medicine;
 import com.pharmacy.Database.Database;
 import com.pharmacy.Controllers.Login;
 import com.pharmacy.Main;
+
+//Javafx imports
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -13,18 +19,18 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+//Java util and org imports
 import org.bson.Document;
-import org.bson.types.ObjectId;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public class Methods {
+
+    //Changing scene methods
     public static void loadStage(String fxml, MouseEvent event) {
         try {
-            System.out.println(fxml);
             Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource(fxml + ".fxml")));
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
@@ -47,6 +53,8 @@ public class Methods {
         }
     }
 
+
+    //Authorisation method
     public static void auth(ActionEvent event, String login , String password) {
         Document founded = Database.users.find(new Document("login", login)).first();
         if (founded != null && password.equals(founded.getString("password"))) {
@@ -66,5 +74,42 @@ public class Methods {
                 }
             });
         }
+    }
+
+    //Registration method
+    public static void registration(String name, String login, String password, String position) {
+        Document document = new Document();
+        document.append("name", name);
+        document.append("login", login);
+        document.append("password", password);
+        document.append("position", position);
+        Database.users.insertOne(document);
+        alert("Success", "New user was added");
+    }
+
+    //Alert method
+    public static void alert(String title, String text){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(text);
+        alert.showAndWait().ifPresent(rs -> {
+            if (rs == ButtonType.OK) {
+                System.out.print("");
+            }
+        });
+    }
+
+
+    //Getting all medicine method
+    public static ObservableList getMeds() {
+        ObservableList<Medicine> meds = FXCollections.observableArrayList();
+        for (Document document : Database.foundedMedicine) {
+            Medicine medicine = new Medicine(
+                    document.getString("name"),
+                    Integer.parseInt(document.getString("quant")),
+                    Integer.parseInt(document.getString("price")));
+            meds.add(medicine);
+        }
+        return meds;
     }
 }
